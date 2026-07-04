85e9d5e2184a0ae65dad1d51a0436ca570de3809
brauck <brauckhoff@yandex.com>
2026-07-04 23:50:59 +0300

    add Todo component

--- moz-todo-react-ts/src/App.tsx
+++ moz-todo-react-ts/src/App.tsx
@@ -2,13 +2,13 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from './assets/vite.svg'
 // import heroImg from './assets/hero.png'
-import './App.css'
+import { TodoProps, Todo } from "./components/Todo";
 
-type AppProps = {
+/*type AppProps = {
   subject: string
-}
+}*/
 
-function App({ subject }: AppProps) {
+/*function App({ subject }: AppProps) {  
   return (
     <>
       <header>
@@ -16,6 +16,65 @@ function App({ subject }: AppProps) {
       </header>
       <button type="button">Click me!</button>
     </>
+  );
+}
+
+export default App;*/
+
+type AppProps = {
+  tasks: TodoProps[]
+}
+
+function App({ tasks }: AppProps) {
+  const taskList = tasks?.map((task) => (
+  <Todo id={task.id} name={task.name} completed={task.completed} />
+  ));
+  return (
+    <div className="todoapp stack-large">
+      {/*<h1 className="visually-hidden">Hello, {subject}!</h1>*/}
+      <h1>TodoMatic</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label__lg">
+            What needs to be done?
+          </label>
+        </h2>
+        <input
+          type="text"
+          id="new-todo-input"
+          className="input input__lg"
+          name="text"
+          autoComplete="off"
+        />
+        <button type="submit" className="btn btn__primary btn__lg">
+          Add
+        </button>
+      </form>
+      <div className="filters btn-group stack-exception">
+        <button type="button" className="btn toggle-btn" aria-pressed="true">
+          <span className="visually-hidden">Show </span>
+          <span>all</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Active</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+        <button type="button" className="btn toggle-btn" aria-pressed="false">
+          <span className="visually-hidden">Show </span>
+          <span>Completed</span>
+          <span className="visually-hidden"> tasks</span>
+        </button>
+      </div>
+      <h2 id="list-heading">3 tasks remaining</h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading">
+        {taskList}
+      </ul>
+    </div>
   );
 }
 
--- moz-todo-react-ts/src/main.tsx
+++ moz-todo-react-ts/src/main.tsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { TodoProps } from "./components/Todo";
+
+const DATA: TodoProps[] = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App subject="Clarice" />
+    <App tasks={DATA} />
   </StrictMode>,
 )
